722b5f20c4241ba6401054a8efff535d9f0694ed
Carsten Bartelt <carsten.bartelt@itdesign.de>
2021-08-11 16:49:14 +0200

    Added GraphQl CreateLink Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import {setContext} from "@apollo/client/link/context";
 
 
 // 1
@@ -17,9 +18,17 @@ const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTYyODY4NzcwMX0.uoObYI-dt5zkGIDUbZ2IoANZYbh-wTExH2qx6wMHRFM',
+        }
+    }
+});
 // 3
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
